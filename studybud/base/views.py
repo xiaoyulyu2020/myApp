e9275b16c7ae0446a7d06a8024de2180ddc0fccc
Xiaoyu Lyu <lyu.xiaoyu2@mail.dcu.ie>
2022-12-28 19:14:47 +0000

    start

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -17,7 +17,8 @@ rooms = [
 
 
 def home(request):
-    return render(request, 'home.html', {'rooms' : rooms})
+    context = {"rooms":rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, "room.html")
