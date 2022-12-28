45b31da9589612f45fa466c2767835f89e1b590d
Xiaoyu Lyu <lyu.xiaoyu2@mail.dcu.ie>
2022-12-28 18:40:20 +0000

    start

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -9,8 +9,15 @@ from django.shortcuts import render
 # def room(request):
 #     return HttpResponse("ROOM")
 
+rooms = [
+    {'id': 1, 'name': "let's learn"},
+    {'id': 2, 'name': "How to get data"},
+    {'id': 3, 'name': "From the views"},
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms' : rooms})
 
 def room(request):
     return render(request, "room.html")
